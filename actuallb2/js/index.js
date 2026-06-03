e4a43a0f03cce5d50a4fe5102ba4fea3e28a4773
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-06-02 23:39:02 -0400

    changed the file names for the render page to work and added textcontent

---
+++ actuallb2/js/index.js
@@ -0,0 +1,17 @@
+let health = 65;
+let ammo = 8;
+let shield = true;
+let missionProgress = 40;
+let enemyNearby = true;
+
+
+const ammov = document.querySelector('#ammo-id');
+const shieldv = document.querySelector('#shield-id');
+const healthv = document.querySelector('#health-id');
+const ememyv = document.querySelector('#enemy-id');
+const progressv = document.querySelector('#progress-id');
+
+ammov.textContent = ammo;
+shieldv.textContent = shield;
+healthv.textContent = health;
+enemyv.textContent = enemyNearby;
--- actuallb2/js/main.js
+++
@@ -1,5 +0,0 @@
-let health = 65;
-let ammo = 8;
-let shield = true;
-let missionProgress = 40;
-let enemyNearby = true;
