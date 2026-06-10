c6c21586b05a380baa382296abfd86fb0b987091
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-06-09 22:54:24 -0400

    added variables for the order of the array elements

--- assignment1/js/index.js
+++ assignment1/js/index.js
@@ -3,3 +3,10 @@ let missionLocation = ["Underwater", "Space Station", "Antarctica", "Forest Ruin
 let weaponLoadout = ["Gun", "Sword", "Bow", "Spear", "Staff", "Wand"];
 let missionObjective = ["Rescue Hostage", "Get Intel", "Spy On Target", "Sabotage Enemy"];
 let riskLevel = ["Low", "Intermediate", "Medium", "High", "Extreme"];
+
+
+let agentTypeIndex = 0;
+let missionLocationIndex = 0;
+let weaponLoadoutIndex = 0;
+let missionObjectiveIndex = 0;
+let riskLevelIndex = 0;
