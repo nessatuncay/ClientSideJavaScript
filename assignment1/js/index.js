3a1e1784e712beb42f0a093cb8d77756a8b48eff
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-06-09 22:45:52 -0400

    added the variables as arrays

---
+++ assignment1/js/index.js
@@ -0,0 +1,5 @@
+let agentType = ["Shadow", "Viper", "Nova", "Ghost"];
+let missionLocation = ["Underwater", "Space Station", "Antarctica", "Forest Ruins"];
+let weaponLoadout = ["Gun", "Sword", "Bow", "Spear", "Staff", "Wand"];
+let missionObjective = ["Rescue Hostage", "Get Intel", "Spy On Target", "Sabotage Enemy"];
+let riskLevel = ["Low", "Intermediate", "Medium", "High", "Extreme"];
