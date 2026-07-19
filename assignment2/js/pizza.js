922733d6ea6abaaa909c6976a4ac6d49f560ff5d
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-07-19 16:54:01 -0400

    created the pizza class and its constructors

---
+++ assignment2/js/pizza.js
@@ -0,0 +1,29 @@
+
+
+class Pizza
+{
+    name;
+    address;
+    phoneNumber;
+    pizzaSize;
+    crustType;
+    mainSauce;
+    toppings;
+    pizzaAmount;
+    spice;
+    specialInstructions;
+
+    constructor (name, address, phoneNumber, pizzaSize, crustType, mainSauce, toppings, pizzaAmount, spice, specialInstructions)
+    {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.pizzaSize = pizzaSize;
+        this.crustType = crustType;
+        this.mainSauce = mainSauce;
+        this.toppings = toppings;
+        this.pizzaAmount = pizzaAmount;
+        this.spice = spice;
+        this.specialInstructions = specialInstructions;
+    }
+}
