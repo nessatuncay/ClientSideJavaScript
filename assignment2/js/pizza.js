4800f9b727eef2b091d0f281b376a131625ab427
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-07-19 20:26:14 -0400

    worked on the pizza class

--- assignment2/js/pizza.js
+++ assignment2/js/pizza.js
@@ -26,4 +26,17 @@ class Pizza
         this.spice = spice;
         this.specialInstructions = specialInstructions;
     }
+
+
+    getOrder()
+    {
+        const toppingAmount = this.toppings.length > 0
+            ? this.toppings.join(", ")
+            : "No Toppings Selected";
+
+        let order = "Order Details:\n Customer Name: ${this.name}\n Customer Address: ${this.address}\n Phone Number: ${this.phoneNumber}\n Pizza Size: ${this.pizzaSize}\n Crust Type: ${this.crustType}\n Main Sauce: ${this.mainSauce}\n Toppings: ${toppingAmount}\n Pizza Amount: ${this.pizzaAmount}\n Spice Level: ${this.spice}\n Special Instructions: ${this.specialInstructions}";
+        
+        return order;
+    }
+
 }
