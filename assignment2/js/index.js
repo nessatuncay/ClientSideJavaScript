e276ba8b4996754896ca57b8f92da655db2d9da5
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-07-19 20:52:09 -0400

    got the values and assigned them to variables

---
+++ assignment2/js/index.js
@@ -0,0 +1,27 @@
+
+const name = document.getElementById("name").value;
+const address = document.getElementById("address").value;
+const phoneNumber = document.getElementById("phone").value;
+const pizzaSize = document.getElementById("size").value;
+const crustType = document.getElementById("crust").value;
+const mainSauce = document.getElementById("sauce").value;
+const pizzaAmount = document.getElementById("pizza-amount").value;
+const spice = document.getElementById("spice").value;
+const specialInstructions = document.getElementById("special-instructions").value;
+const toppings = Array.from(document.querySelectorAll
+                 ('input[class="toppings"]:checked'))
+                 .map((checkbox) => checkbox.value);
+
+return new Pizza
+{
+    name;
+    address;
+    phoneNumber;
+    pizzaSize;
+    crustType;
+    mainSauce;
+    toppings;
+    pizzaAmount;
+    spice;
+    specialInstructions;
+}
